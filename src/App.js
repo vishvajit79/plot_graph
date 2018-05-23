c47aec548611a7a9a7a2f7cfbb9db1bc09f270e2
Vishvajit Kher <vishvajit79@gmail.com>
2018-05-22 22:54:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import History from './components/History';
+import Graph from './components/Graph';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Function Plot Application</h1>
+        </header>
+        <div className="row">
+          <div className="col-md-8">
+            <Graph/>
+          </div>
+          <div className="col-md-4">
+            <History/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
