e0907a8c513b3d1ed1535eb1ca2f53c92b46ce9a
Vishvajit Kher <vishvajit79@gmail.com>
2018-05-23 00:16:32 -0400

    graph plot now working except for boundaries

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -4,61 +4,97 @@ import functionPlot from 'function-plot';
 
 window.d3 = d3;
 
-functionPlot({
-  title: 'y = x * x',
-  target: '#quadratic-with-options',
-  width: 580,
-  height: 400,
-  disableZoom: true,
-  xAxis: {
-    label: 'x - axis',
-    domain: [-6, 6]
-  },
-  yAxis: {
-    label: 'y - axis'
-  },
-  data: [{
-    fn: 'sin(x)'
-  }]
-});
-
-
 class Graph extends Component {
   constructor(props){
     super(props);
     this.state = {
       equation: '',
-      boundaries: ''
+      boundaries: [-6,6],
+      errorMessage: ''
     };
   }
   
+  graphFunction = () => {
+    if(this.state.equation) {
+      try {
+        functionPlot({
+          target: '#quadratic-with-options',
+          width: 400,
+          height: 400,
+          disableZoom: true,
+          xAxis: {
+            label: 'x - axis',
+            domain: this.state.boundaries
+          },
+          yAxis: {
+            label: 'y - axis'
+          },
+          data: [{
+            fn: this.state.equation
+          }]
+        });
+      }
+      catch (e) {
+        this.setState({
+          errorMessage: 'Invalid input values'
+        })
+      }
+    }
+  };
+  
   handleEquationChange = (event) => {
     const equation = event.target.value;
-    this.setState({
-      equation: equation
-    });
+    if(equation) {
+      this.setState({
+        equation: equation,
+        errorMessage: ''
+      });
+    }
   };
   
-  handleBoundariesChange = (event) => {
+  handleBoundaryChange = (event) => {
     const boundaries = event.target.value;
-    this.setState({
-      boundaries: boundaries
-    });
+    if(boundaries) {
+      console.log(event.target.value);
+      console.log(JSON.parse(event.target.value));
+      this.setState({
+        boundaries: boundaries,
+        errorMessage: ''
+      });
+    }
   };
   
-  handleHistorySave = () => {
-  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const equation = event.target.elements.equation.value;
+    const boundaries = event.target.elements.boundaries.value;
+    if(equation) {
+      this.setState({
+        equation: equation,
+        errorMessage: ''
+      });
+    }
+    if(boundaries) {
+      this.setState({
+        boundaries: boundaries,
+        errorMessage: ''
+      });
+    }
+    this.graphFunction();
   };
   
   render() {
     return(
       <div>
         <h2 className="App-title">Graph</h2>
-        <input type="text" name="equation" className="question" onChange={this.handleEquationChange}/>
-        <label htmlFor="equation"><span>Equation...</span></label>
-        <input name="boundaries" className="question" onChange={this.handleBoundariesChange}/>
-        <label htmlFor="boundaries"><span>Boundaries...</span></label>
-        <button className="btn btn-outline-dark m-3" onClick={this.handleHistorySave}>Save in history</button>
+        { this.state.errorMessage && <p className="text-danger">{ this.state.errorMessage }</p>}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="equation" className="question" onChange={this.handleEquationChange} required/>
+          <label htmlFor="equation"><span>Equation...sin(x)</span></label>
+          <input type="text" name="boundaries" className="question" onChange={this.handleBoundaryChange}/>
+          <label htmlFor="boundaries"><span>Boundaries...[-6,6]</span></label>
+          <button className="btn btn-outline-dark m-3">Draw</button>
+        </form>
       </div>
     );
   }
